22f08852e512a5d334f85f424743988bdd977fa0
abnerFSilva <abner.fsilva@gmail.com>
2022-05-21 13:14:54 -0300

    recreate folder

---
+++ Abner Electron/public/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+require("@electron/remote/main").initialize();
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+  win.loadURL("http://localhost:3000");
+
+  app.on("ready", createWindow());
+
+  //Quit when all windows are closed. on MacOS, this is the "Cmd + Q" shortcut.
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+  app.on("activate", () => {
+    //On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+}
